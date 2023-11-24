cde294a9b9af6cff6072eb4917f128cd4ba3fd71
vaneIv <appvane@gmail.com>
2023-11-24 17:50:06 +0100

    Created MusicNotificationManager and MusicPlayerNotificationListener

--- app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
@@ -3,4 +3,7 @@ package com.plcoding.spotifycloneyt.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
