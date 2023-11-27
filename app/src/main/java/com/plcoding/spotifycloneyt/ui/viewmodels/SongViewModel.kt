6d3cee2eaf973093938d5442a3a656396ba9d1b9
vaneIv <appvane@gmail.com>
2023-11-27 09:15:42 +0100

    Added SongViewModel to get the current position of the player to configure the position of our seekbar.

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/ui/viewmodels/SongViewModel.kt
@@ -0,0 +1,40 @@
+package com.plcoding.spotifycloneyt.ui.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.spotifycloneyt.exoplayer.MusicService
+import com.plcoding.spotifycloneyt.exoplayer.MusicServiceConnection
+import com.plcoding.spotifycloneyt.exoplayer.currentPlaybackPosition
+import com.plcoding.spotifycloneyt.other.Constants.UPDATE_PLAYER_POSITION_INTERVAL
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SongViewModel @ViewModelInject constructor(
+    musicServiceConnection: MusicServiceConnection
+) : ViewModel() {
+
+    private val playbackState = musicServiceConnection.playbackState
+
+    private val _curSongDuration = MutableLiveData<Long>()
+    val curSongDuration: LiveData<Long> = _curSongDuration
+
+    private val _curPlayerPosition = MutableLiveData<Long>()
+    val curPlayerPosition: LiveData<Long> = _curPlayerPosition
+
+    private fun updateCurrentPlayerPosition() {
+        viewModelScope.launch {
+            while (true) {
+                val position = playbackState.value?.currentPlaybackPosition
+                if (curPlayerPosition.value != position) {
+                    _curPlayerPosition.postValue(position)
+                    _curSongDuration.postValue(MusicService.curSongDuration)
+                }
+                delay(UPDATE_PLAYER_POSITION_INTERVAL)
+            }
+        }
+    }
+
+}
